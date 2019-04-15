4257c8223e000e235445eff2bfbe789b2631f1f2
Michael <darmodymichael@gmail.com>
2019-04-15 16:10:06 +0100

    Added CSS to boarder the image of a cat

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  "<div style='border: 3px dashed red'>
+       <img src='http://bit.ly/1eze8aE'>
+     </div>"
 end
 set :session_secret, 'super secret'
