4ef9c614385dc49e07fad9e4fac3d2646c5e67bf
Michael <darmodymichael@gmail.com>
2019-04-16 15:38:28 +0100

    Added and completed form.erb file

--- app.rb
+++ app.rb
@@ -8,8 +8,18 @@ get '/secret' do
   "The secret response"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/form' do
+  erb(:form)
+end
 set :session_secret, 'super secret'
